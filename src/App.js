0624287a585ae05c4d9b7c2ddc75d6b72ce4d079
Shokhmurod <shokhpjwstk@gmail.com>
2022-12-28 05:15:13 +0100

    Redirecting the user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
@@ -15,6 +15,9 @@ function App() {
       </header>
       <main>
         <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome"></Redirect>
+          </Route>
           <Route path="/welcome" exact>
             <Welcome />
           </Route>
