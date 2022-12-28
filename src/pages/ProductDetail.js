e3d9a9622c50297bdf1a32d4fec5e28f142bea6b
Shokhmurod <shokhpjwstk@gmail.com>
2022-12-28 01:56:39 +0100

    Extracted route params via useParams

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
+  const params = useParams();
   return (
     <section>
       <h2>Product Detail</h2>
+      <p>{params.productId}</p>
     </section>
   );
 };
